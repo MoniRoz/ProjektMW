04b4340507a36a69614c0770e7bdf2e2318a9a4e
MoniRoz <monika.rozwadowska@outlook.com>
2017-01-11 03:50:35 +0100

    walidatory

--- Frontend/src/app/components/rejestracja-samochodu/rejestracja-samochodu.component.ts
+++ Frontend/src/app/components/rejestracja-samochodu/rejestracja-samochodu.component.ts
@@ -12,23 +12,23 @@ export class RejestracjaSamochodu implements OnChanges {
   @Output() notify = new EventEmitter();
   private formularzRejestracji: FormGroup;
   private samochod: Samochod;
-  // private samochod: Samochod = new Samochod('Samochod', 'Ford', 'sedan', 'Focus',
-  //   '2008', 'W0L0XCF0814000002', 'F16D3000080K', 'WD70757', '23423456', '200000', 'czerwony');
+  // private samochod: Samochod = new Samochod('Osobowy', 'Ford', 'Sedan', 'Focus',
+  //   '2008', 'W0L0XCF0814000002', 'F16D3000080K', 'WD70757', 'AWD23423456AAAA', '200000', 'Czerwony');
 
   constructor(private fb: FormBuilder,
               private service: StarostaService) {
     this.formularzRejestracji = fb.group({
-      'rodzaj_pojazdu': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
-      'marka': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
-      'typ': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
-      'model': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
-      'rok_produkcji': [null, [Validators.required, Validators.minLength(4), Validators.pattern('[0-9]{4,4}')]],
-      'nr_VIN': [null, [Validators.required, Validators.pattern('[A-Z0-9]*')]],
-      'nr_silnika': [null, [Validators.required, Validators.pattern('[A-Z0-9]*')]],
-      'd_nr_rejestracyjny': [null, [Validators.required, Validators.pattern('[A-Z0-9]*')]],
-      'nr_kart_pojazdu': [null, [Validators.required, Validators.pattern('[0-9]*')]],
+      'rodzaj_pojazdu': [null, [Validators.required, Validators.pattern('^[A-Z][a-z]+')]],
+      'marka': [null, [Validators.required, Validators.pattern('^[A-Z][a-z]+')]],
+      'typ': [null, [Validators.required, Validators.pattern('^[A-Z][a-z]+')]],
+      'model': [null, [Validators.required, Validators.pattern('^[A-Z][a-z]+')]],
+      'rok_produkcji': [null, [Validators.required, Validators.pattern('^[1-2][0-9]{3}$')]],
+      'nr_VIN': [null, [Validators.required, Validators.pattern('^[0-9A-HJ-NPR-Z]{17}$')]],
+      'nr_silnika': [null, [Validators.required, Validators.pattern('^[A-Z0-9]{12}$')]],
+      'd_nr_rejestracyjny': [null, [Validators.required, Validators.pattern('^[A-Z0-9]{7}$')]],
+      'nr_kart_pojazdu': [null, [Validators.required, Validators.pattern('^[A-Z0-9]{15}$'|| 'nie wydano')]],
       'przebieg_p_w_km': [null, [Validators.required, Validators.pattern('[0-9]*')]],
-      'barwa_nadwozia': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]]
+      'barwa_nadwozia': [null, [Validators.required, Validators.pattern('^[A-Z][a-z]+')]]
     });
 
     this.formularzRejestracji.valueChanges.subscribe(() => {
--- Frontend/src/app/components/wlasciciel/wlasciciel-formularz.component.ts
+++ Frontend/src/app/components/wlasciciel/wlasciciel-formularz.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Output, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {FormGroup, FormBuilder, Validators, Validator} from "@angular/forms";
 import {Wlasciciel} from "../../_mocks/wlasciciel";
 import {StarostaService} from "../../_services/starosta.service";
 
@@ -12,24 +12,28 @@ export class FormularzWlasciciela implements OnChanges {
   @Output() notify = new EventEmitter();
 
   private formularzWlasciciela: FormGroup;
-  // private wlasciciel: Wlasciciel = new Wlasciciel('John', 'Doe', 'Kocia 11', '3', '03-028', 'Warszawa', '75120514389');
+  // private wlasciciel: Wlasciciel = new Wlasciciel('John', 'Doe-Ave', 'Kocia', '3', '03-028', 'Warszawa', '75120514389');
   private wlasciciel: Wlasciciel;
 
   constructor(private fb: FormBuilder,
               private service: StarostaService) {
     this.formularzWlasciciela = fb.group({
-      'imie': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
-      'nazwisko': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
-      'ulica': [null],
-      'nr_domu': [null],
-      'kod_pocztowy': [null],
-      'miejscowosc': [null],
-      'pesel': [null]
+      'imie': [null, [Validators.required, Validators.pattern('^[A-ZŁŻ][a-ząćęłńóśźżĄĘŁŃÓŚŹŻ]{1,20}$')]],
+      'nazwisko': [null, [Validators.required, Validators.pattern('^[A-ZĆŁŚŻŹ][a-ząćęłńóśźżĄĘŁŃÓŚŹŻ]{1,20}((-|\\s)?[A-ZĆŁŚŻŹ][a-ząćęłńóśźżĄĘŁŃÓŚŹŻ]{1,20})?$')]],
+      'ulica': [null, [Validators.required, Validators.pattern('^[A-ZŁŻ][a-ząćęłńóśźżĄĘŁŃÓŚŹŻ]{1,20}$')]],
+      'nr_domu': [null, [Validators.required, Validators.pattern('^[0-9]{1,3}[a-z]?$')]],
+      'kod_pocztowy': [null, [Validators.required, Validators.pattern('^[0-9]{2}-[0-9]{3}$')]],
+      'miejscowosc': [null, [Validators.required, Validators.pattern('^[A-ZĆŁŚŻŹ][a-ząćęłńóśźżĄĘŁŃÓŚŹŻ]{1,20}((-|\\s)?[A-ZĆŁŚŻŹ][a-ząćęłńóśźżĄĘŁŃÓŚŹŻ]{1,20})?$')]],
+      'pesel': [null, [Validators.required, Validators.pattern('^\\d{11}$')]]
     });
 
     this.formularzWlasciciela.valueChanges.subscribe(() => {
       this.notify.emit(this.formularzWlasciciela);
     });
+  }
+
+  check(value: any) {
+    console.log(value);
   }
 
   isOwnerExists(value: any) {
