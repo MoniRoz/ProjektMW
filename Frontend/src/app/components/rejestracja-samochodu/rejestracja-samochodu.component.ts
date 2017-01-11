25f3afb32e9ea0a6900cea20857a150d8facf8c3
MoniRoz <monika.rozwadowska@outlook.com>
2017-01-11 02:22:35 +0100

    binding pol wyszukiwania

--- Frontend/src/app/components/rejestracja-samochodu/rejestracja-samochodu.component.ts
+++ Frontend/src/app/components/rejestracja-samochodu/rejestracja-samochodu.component.ts
@@ -1,28 +1,32 @@
 import {Component, Output, EventEmitter, Input, SimpleChanges, OnChanges} from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {Samochod} from "../../_mocks/samochod";
 
 @Component({
   selector: 'rejestracja-samochodu',
   templateUrl: 'rejestracja-samochodu.component.html'
 })
-export class RejestracjaSamochodu implements OnChanges{
+export class RejestracjaSamochodu implements OnChanges {
   @Input() reset: boolean;
   @Output() notify = new EventEmitter();
-  formularzRejestracji: FormGroup;
+  private formularzRejestracji: FormGroup;
+  //private samochod: Samochod;
+  private samochod: Samochod = new Samochod('Samochod', 'Ford', 'sedan', 'Focus',
+    '2008', 'W0L0XCF0814000002', 'F16D3000080K', 'WD70757', '23423456', '200000', 'czerwony');
 
   constructor(private fb: FormBuilder) {
     this.formularzRejestracji = fb.group({
-      'rodzaj_pojazdu': [null, [Validators.required,Validators.pattern('[a-zA-Z]*')]],
-      'marka': [null, [Validators.required,Validators.pattern('[a-zA-Z]*')]],
-      'typ': [null, [Validators.required,Validators.pattern('[a-zA-Z]*')]],
-      'model': [null, [Validators.required,Validators.pattern('[a-zA-Z]*')]],
-      'rok_produkcji': [null, [Validators.required,Validators.minLength(4), Validators.pattern('[0-9]{4,4}')]],
-      'nr_VIN': [null, [Validators.required,Validators.pattern('[A-Z0-9]*')]],
-      'nr_silnika': [null, [Validators.required,Validators.pattern('[A-Z0-9]*')]],
-      'd_nr_rejestracyjny': [null, [Validators.required,Validators.pattern('[A-Z0-9]*')]],
-      'nr_kart_pojazdu': [null, [Validators.required,Validators.pattern('[0-9]*')]],
-      'przebieg_p_w_km': [null, [Validators.required,Validators.pattern('[0-9]*')]],
-      'barwa_nadwozia': [null, [Validators.required,Validators.pattern('[a-zA-Z]*')]]
+      'rodzaj_pojazdu': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      'marka': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      'typ': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      'model': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      'rok_produkcji': [null, [Validators.required, Validators.minLength(4), Validators.pattern('[0-9]{4,4}')]],
+      'nr_VIN': [null, [Validators.required, Validators.pattern('[A-Z0-9]*')]],
+      'nr_silnika': [null, [Validators.required, Validators.pattern('[A-Z0-9]*')]],
+      'd_nr_rejestracyjny': [null, [Validators.required, Validators.pattern('[A-Z0-9]*')]],
+      'nr_kart_pojazdu': [null, [Validators.required, Validators.pattern('[0-9]*')]],
+      'przebieg_p_w_km': [null, [Validators.required, Validators.pattern('[0-9]*')]],
+      'barwa_nadwozia': [null, [Validators.required, Validators.pattern('[a-zA-Z]*')]]
     });
 
     this.formularzRejestracji.valueChanges.subscribe(() => {
@@ -33,6 +37,10 @@ export class RejestracjaSamochodu implements OnChanges{
   ngOnChanges(changes: SimpleChanges) {
     if (this.reset) {
       this.formularzRejestracji.reset();
+    }
+
+    if (!this.samochod) {
+      this.samochod = new Samochod(null, null, null, null, null, null, null, null, null, null, null);
     }
   }
 }
--- Frontend/src/app/components/wlasciciel/wlasciciel-formularz.component.ts
+++ Frontend/src/app/components/wlasciciel/wlasciciel-formularz.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Output, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {Wlasciciel} from "../../_mocks/wlasciciel";
 
 @Component({
   selector: 'wlasciciel-formularz',
@@ -10,6 +11,8 @@ export class FormularzWlasciciela implements OnChanges {
   @Output() notify = new EventEmitter();
 
   private formularzWlasciciela: FormGroup;
+  private wlasciciel: Wlasciciel = new Wlasciciel('John', 'Doe', 'Kocia 11', '3', '03-028', 'Warszawa', '75120514389');
+  // private wlasciciel: Wlasciciel;
 
   constructor(private fb: FormBuilder) {
     this.formularzWlasciciela = fb.group({
@@ -31,6 +34,8 @@ export class FormularzWlasciciela implements OnChanges {
     if (this.reset) {
       this.formularzWlasciciela.reset();
     }
+    if (!this.wlasciciel) {
+      this.wlasciciel = new Wlasciciel(null, null, null, null, null, null, null);
+    }
   }
-
 }
